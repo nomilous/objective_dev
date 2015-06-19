50dc3a5eaefb04431d5ce767b10ddab7c59bde84
nomilous <richard@clue.co.za>
2015-06-19 11:38:56 +0200

    mock supports multiple functions from args

--- example/spec/js_spec.js
+++ example/spec/js_spec.js
@@ -4,20 +4,28 @@ objective(function() {
     mock('thing');
   })
 
-  it.only('uses function name for mock if no list provided', function(thing) {
-      
+  xit('uses function name for mock if no list provided', function(thing) {
+    
+
+    var got = mock('got').does(function have() {});
+
     thing.does(
-      function configure() {},
+      function configure() {
+        return thing;
+      },
       function get(){
-        return mock('got').does(
-          function have() {
-            /* carried away */
-          }
-        );
+        return got;
+        // mock('got').does(
+        //   function have() {
+        //     /* expectation created inside expectation will only */
+        //     /* be activated if the outer expectation is met */
+        //   }
+        // );
       }
     );
 
-    // thing.get();
+    thing.configure() //.get().have();
+    // ExpectationError: Missing call(s) to thing.get(), got.have()
 
   });
 });
